2b4e0c13993ad106c02602cf0a99c5f6811eb661
Tim Mardesen <timothymardesen@yahoo.com>
2020-02-11 10:42:03 -0600

    Added better spinner (packman)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from '../../node_modules/ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Timothy-Mardesen-Portfolio';
-  //stuff = `Angular ${VERSION.full} stuff`;
-  //int lsl = 5;
+
+  constructor(private spinner: NgxSpinnerService) { }
+
+  ngOnInit() {
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    },
+    2000);
+  }
+  // stuff = `Angular ${VERSION.full} stuff`;
+  // int lsl = 5;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SquareComponent } from './square/square.component';
+import { NgxSpinnerModule } from '../../node_modules/ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SquareComponent } from './square/square.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
